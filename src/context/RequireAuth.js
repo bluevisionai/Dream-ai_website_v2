1b565e5f0e472de0688e113e892f3c154222fca9
Mashudu <mashudu@bluevisionai.co.za>
2024-01-09 13:37:28 +0200

    jwt update

--- src/context/RequireAuth.js
+++ src/context/RequireAuth.js
@@ -1,22 +1,29 @@
 import { useLocation, Navigate, Outlet } from "react-router-dom";
 import useAuth from "../hooks/useAuth";
-import { jwtDecode } from "jwt-decode";
+// import { jwtDecode } from "jwt-decode";
+import { useJwt } from "react-jwt";
 
 const RequireAuth = ({allowedRoles}) => {
     const {auth} = useAuth();
     const location = useLocation();
     const accessToken = auth?.accessToken;
 
+    const { decodedToken } = useJwt(accessToken);
+    let decoded = decodedToken
+    // console.error("decodedToken: ", JSON.stringify(decodedToken));
+    console.log("decoded: ", decoded?.role);
 
-    let decode;
-    try {
-        decode = accessToken ? jwtDecode(accessToken) : undefined;
-    } catch (error) {
-        console.error("Error decoding access token:", error);
-        decode = undefined;
-    }
+    // let decode;
+    // try {
+    //     decode = accessToken ? jwtDecode(accessToken) : undefined;
+        
+
+    // } catch (error) {
+    //     console.error("Error decoding access token:", error);
+    //     decode = undefined;
+    // }
     
-    const roles = decode?.role ? [decode.role] : [];
+    const roles = decoded?.role ? [decoded.role] : [];
 
     return (
         roles.find(role => allowedRoles?.includes(role))
